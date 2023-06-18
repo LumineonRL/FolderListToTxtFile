08bdf426635beb512ea518ea160724ff14d274a0
LumineonRL <thebaconator25@aim.com>
2023-06-18 18:17:26 -0400

    Method access and file_prefix added.

--- FolderListToTxtFile.py
+++ FolderListToTxtFile.py
@@ -9,20 +9,20 @@ class FileFinder:
         extensions_to_check: list[str],
         input_paths_file: str,
         output_path_file: str,
+        file_prefix: str
     ):
         self.input_paths_file = input_paths_file
         self.output_path_file = output_path_file
         self.encoding = "utf-8"
         self.extensions_to_check = extensions_to_check
+        self.file_prefix = file_prefix
 
     def get_input_dirs(self) -> list[str]:
-        """Reads in list of input directories from InputPaths file"""
         with open(self.input_paths_file, "r", encoding=self.encoding) as f:
             input_dirs = [line.strip() for line in f]
         return input_dirs
 
     def get_output_dir(self) -> str:
-        """Reads in output directory path from OutputPath file"""
         with open(self.output_path_file, "r", encoding=self.encoding) as f:
             output_dir = f.readline().strip()
         return output_dir
@@ -30,22 +30,18 @@ class FileFinder:
     def write_files_paths_to_txt_file(
         self, input_dirs: list[str], extensions_to_check: list[str], output_dir: str
     ) -> None:
-        """Generates an appropriate name for the textfile based on current date/time and calls another
-        function to actually save paths into this newly generated textfile."""
-
-        all_file_paths = self.find_all_files_in_directories(input_dirs)
-        filtered_paths = self.filter_valid_file_extensions(all_file_paths)
+        all_file_paths = self._find_all_files_in_directories(input_dirs)
+        filtered_paths = self._filter_valid_file_extensions(all_file_paths)
 
         if len(filtered_paths) > 0:
-            output_filename = self.generate_output_filename_with_timestamp()
+            output_filename = self._generate_output_filename_with_timestamp()
             output_filepath = os.path.join(output_dir, output_filename)
 
-            self.write_list_to_file(filtered_paths, output_filepath)
+            self._write_list_to_file(filtered_paths, output_filepath)
         else:
             print("No files found.")
 
-    def find_all_files_in_directories(self, input_dirs: list[str]) -> list[str]:
-        """Finds all files recursively within given directories."""
+    def _find_all_files_in_directories(self, input_dirs: list[str]) -> list[str]:
         files = []
 
         for dir_path in input_dirs:
@@ -54,24 +50,18 @@ class FileFinder:
 
         return files
 
-    def filter_valid_file_extensions(self, file_paths: list[str]) -> list[str]:
-        """Filters out paths with file extensions not in our allowed set."""
-        valid_files = [f for f in file_paths if self.is_file_of_specified_extension(f)]
+    def _filter_valid_file_extensions(self, file_paths: list[str]) -> list[str]:
+        valid_files = [f for f in file_paths if self._is_file_of_specified_extension(f)]
 
         return valid_files
 
-    def is_file_of_specified_extension(self, file_path: str) -> bool:
-        """Checks if a given file has an extension that matches one or more of the specified extensions."""
-
-        # Get lower-cased filename extension from filepath using get_file_extension helper method.
-        ext = self.get_file_extension(file_path)
-
-        # Check whether obtained filename-extension exists in our defined set/list of allowed image-extensions.
-        result = self.is_extension_in_list(ext)
+    def _is_file_of_specified_extension(self, file_path: str) -> bool:
+        ext = self._get_file_extension(file_path)
+        result = self._is_extension_in_list(ext)
 
         return result
 
-    def get_file_extension(self, file_path):
+    def _get_file_extension(self, file_path):
         """
         Returns the extension of a file (including . separator) as a string.
         """
@@ -79,27 +69,20 @@ class FileFinder:
 
         return ext.lower()
 
-    def is_extension_in_list(self, extension):
-        """
-        Checks if an extension exists in our list of valid extensions.
-        Returns True or False depending on whether it exists or not.
-        """
-
+    def _is_extension_in_list(self, extension):
         return extension in self.extensions_to_check
 
-    def generate_output_filename_with_timestamp(self) -> str:
-        """Returns an output filename with timestamp appended"""
+    def _generate_output_filename_with_timestamp(self) -> str:
         timestamp_str = datetime.now().strftime("%Y%m%d_%H%M%S")
-        return "files_" + timestamp_str + ".txt"
+        
+        return f"{self.file_prefix}{timestamp_str}.txt" 
 
-    def write_list_to_file(self, file_list: list[str], output_path: str) -> None:
-        """Writes a list of strings to a txt file at the specified output path."""
+    def _write_list_to_file(self, file_list: list[str], output_path: str) -> None:
         with open(output_path, "w", encoding=self.encoding) as f:
             for item in file_list:
                 encoded_item = item.encode(self.encoding)
                 decoded_item = encoded_item.decode(self.encoding)
 
-                # Finally write the decoded string onto our desired txt-file.
                 f.write("%s\n" % decoded_item)
 
 
@@ -107,11 +90,13 @@ if __name__ == "__main__":
     extensions = [".jpg", ".jpeg", ".png", ".gif", ".webm"]
     input_path_file = "InputPaths.txt"
     output_path_file = "OutputPath.txt"
+    file_prefix = "files_"
 
     finder = FileFinder(
         extensions_to_check=extensions,
         input_paths_file=input_path_file,
         output_path_file=output_path_file,
+        file_prefix=file_prefix
     )
 
     input_dirs = finder.get_input_dirs()
