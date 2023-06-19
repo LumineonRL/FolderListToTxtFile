3e90ed7e50807d4a5330e0fd6127b379f3b876d2
LumineonRL <thebaconator25@aim.com>
2023-06-19 00:56:26 -0400

    encoding validation added.

--- main.py
+++ main.py
@@ -26,6 +26,114 @@ def parse_arguments() -> argparse.Namespace:
 
     return parser.parse_args()
 
+def run_validation(args: argparse.Namespace) -> None:
+    if not validate_encoding(args.encoding):
+        print(f"{args.encoding} is NOT a valid codec name.")
+
+
+def validate_encoding(encoding_to_validate: str) -> bool:
+    valid_encodings = [
+        "ascii",
+        "big5",
+        "big5hkscs",
+        "cp037",
+        "cp273",
+        "cp424",
+        "cp437",
+        "cp500",
+        "cp720",
+        "cp737",
+        "cp775",
+        "cp850",
+        "cp852",
+        "cp855",
+        "cp856",
+        "cp857",
+        "cp858",
+        "cp860",
+        "cp861",
+        "cp862",
+        "cp863",
+        "cp864",
+        "cp865",
+        "cp866",
+        "cp869",
+        "cp874",
+        "cp875",
+        "cp932",
+        "cp949",
+        "cp950",
+        "cp1006",
+        "cp1026",
+        "cp1125",
+        "cp1140",
+        "cp1250",
+        "cp1251",
+        "cp1252",
+        "cp1253",
+        "cp1254",
+        "cp1255",
+        "cp1256",
+        "cp1257",
+        "cp1258",
+        "euc-jp",
+        "euc-jis-2004",
+        "euc-jisx0213",
+        "euc-kr",
+        "gb2312",
+        "gbk",
+        "gb18030",
+        "hz",
+        "iso2022-jp",
+        "iso2022-jp-1",
+        "iso2022-jp-2",
+        "iso2022-jp-2004",
+        "iso2022-jp-3",
+        "iso2022-jp-ext",
+        "iso2022-kr",
+        "latin-1",
+        "iso8859-2",
+        "iso8859-3",
+        "iso8859-4",
+        "iso8859-5",
+        "iso8859-6",
+        "iso8859-7",
+        "iso8859-8",
+        "iso8859-9",
+        "iso8859-10",
+        "iso8859-11",
+        "iso8859-13",
+        "iso8859-14",
+        "iso8859-15",
+        "iso8859-16",
+        "johab",
+        "koi8-r",
+        "koi8-t",
+        "koi8-u",
+        "kz1048",
+        "mac-cyrillic",
+        "mac-greek",
+        "mac-iceland",
+        "mac-latin2",
+        "mac-roman",
+        "mac-turkish",
+        "ptcp154",
+        "shift-jis",
+        "shift-jis-2004",
+        "shift-jisx0213",
+        "utf-32",
+        "utf-32-be",
+        "utf-32-le",
+        "utf-16",
+        "utf-16-be",
+        "utf-16-le",
+        "utf-7",
+        "utf-8",
+        "utf-8-sig",
+    ]
+
+    return encoding_to_validate in valid_encodings
+
 
 def instantiate_finder(args: argparse.Namespace) -> FileFinder:
     return FileFinder(
@@ -50,5 +158,6 @@ def write_files(finder: FileFinder):
 
 if __name__ == "__main__":
     my_args = parse_arguments()
+    run_validation(my_args)
     my_finder = instantiate_finder(my_args)
     write_files(my_finder)
